2e4fd6df2fe02ac0889498ca284faf11366e4c2e
VictorClvtt <joaoclivatti@hotmail.com>
2025-09-01 17:29:30 -0300

    production update

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("DJANGO_SECRET_KEY", default="chave-insegura")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
